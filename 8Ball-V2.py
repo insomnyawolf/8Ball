015f56e22a2bb09cffd0739f7831bcbe87aefe68
insomnyawolf <ruben.tokyo1999@gmail.com>
2016-04-06 09:59:40 +0200

    Update 8Ball-V2.py
    Added more answers

--- 8Ball-V2.py
+++ 8Ball-V2.py
@@ -3,7 +3,17 @@ print("Original code By insomnyawolf")
 print("Recoded and optimized by ohmyginger")
 print("Hi... Im the magic 8Ball")
 quest = input("Whats your question?\n")
-ans = ["Yes", "No", "Maybe"]
+ans = []
+#Negative ans
+ans.append("Doubtful...")
+ans.append("No")
+ans.append("Never (╯°□°）╯︵ ┻━┻")
+#Neutral ans
+ans.append("Maybe...")
+#possitive ans
+ans.append("Yes")
+ans.append("Not sure, likely yes :)")
+ans.append("Yes, definitely!")
 while True:
     if quest.lower() in ["stop", "quit", "exit"]:
         break
